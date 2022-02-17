3c306e5cda10000e09b69ec24f450668e3c07b69
g_rawol_f <hihiyena328@gmail.com>
2022-02-17 21:54:39 +0900

    Add homepage

--- src/index.js
+++ src/index.js
@@ -18,17 +18,11 @@ import ReduxThunk from 'redux-thunk';
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-    <Provider
-        store={createStoreWithMiddleware(
-            Reducer,
-            window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-        )}
-    >
-        <BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter basename={"https://g-raywol-f.github.io/fab-word-checker"}>
             <App />
         </BrowserRouter>
-    </Provider>
+    </React.StrictMode>
     , document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
